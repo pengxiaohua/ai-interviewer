d897ff6550acc2eb1d0b9d328ed680333b161e3f
Xiaohua Peng <609370075@qq.com>
2025-04-17 01:30:36 +0800

    init project

---
+++ backend/controller/src/main/java/com/xiaohua/controller/HelloController.java
@@ -0,0 +1,20 @@
+/**
+ * @ClassName HelloController
+ * @Auth pengxiaohua
+ * @Date 2025/4/17
+ * @Description: TODO
+ */
+
+package com.xiaohua.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public Object hello() {
+        return "Hello Xiaohuapeng";
+    }
+}
